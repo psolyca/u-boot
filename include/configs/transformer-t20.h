77bc193884bb32508df1c7670f94dbadae0fd3ca
Svyatoslav Ryhel <clamor95@gmail.com>
2022-01-04 11:16:52 +0200

    tf-t20: use common header
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- include/configs/transformer-t20.h
+++ include/configs/transformer-t20.h
@@ -10,23 +10,14 @@
 #include <linux/sizes.h>
 
 #include "tegra20-common.h"
-
-/* High-level configuration options */
-#define CONFIG_TEGRA_BOARD_STRING	"ASUS Transformer"
+#include "transformer-common.h"
 
 #define BOARD_EXTRA_ENV_SETTINGS \
-	"script_addr_r=0x1000\0" \
-	"kernel_addr_r=0x1000000\0" \
-	"dtb_addr_r=0x2000000\0" \
-	"ramdisk_addr_r=0x3000000\0" \
-	"kernel_file=vmlinuz\0" \
-	"dtb_file="CONFIG_DEFAULT_DEVICE_TREE".dtb\0" \
-	"ramdisk_file=uInitrd\0" \
-	"bootloader_file=u-boot-dtb-tegra.bin\0" \
-	"bootkernel=bootz ${kernel_addr_r} - ${dtb_addr_r}\0" \
-	"bootrdkernel=bootz ${kernel_addr_r} ${ramdisk_addr_r} ${dtb_addr_r}\0"
+	MEM_LAYOUT_ENV_SETTINGS \
+	TRANSFORMER_T20_EMMC_LAYOUT \
+	TRANSFORMER_DEFAULT_FILESET \
+	TRANSFORMER_BOOTZ
 
-/* Configurate from PER on eMMC */
 #undef CONFIG_BOOTCOMMAND
 #define CONFIG_BOOTCOMMAND \
 	"if button VolDown;" \
@@ -35,7 +26,7 @@
 		"echo Loading Kernel;" \
 		"if load mmc 1:1 ${kernel_addr_r} ${kernel_file};" \
 		"then echo Loading DTB;" \
-			"load mmc 1:1 ${dtb_addr_r} ${dtb_file};" \
+			"load mmc 1:1 ${fdt_addr_r} ${fdtfile};" \
 			"setenv bootargs console=ttyS0,115200n8 root=/dev/mmcblk1p2 rw gpt;" \
 			"echo Loading Initramfs;" \
 			"if load mmc 1:1 ${ramdisk_addr_r} ${ramdisk_file};" \
@@ -48,7 +39,7 @@
 			"echo Loading Kernel;" \
 			"load mmc 0:1 ${kernel_addr_r} ${kernel_file};" \
 			"echo Loading DTB;" \
-			"load mmc 0:1 ${dtb_addr_r} ${dtb_file};" \
+			"load mmc 0:1 ${fdt_addr_r} ${fdtfile};" \
 			"setenv bootargs console=ttyS0,115200n8 root=/dev/mmcblk0p8 rw gpt;" \
 			"echo Loading Initramfs;" \
 			"if load mmc 0:1 ${ramdisk_addr_r} ${ramdisk_file};" \
@@ -63,12 +54,7 @@
 #define CONFIG_TEGRA_ENABLE_UARTD
 #define CONFIG_SYS_NS16550_COM1		NV_PA_APB_UARTD_BASE
 
-/* Skip EFI check for eMMC */
-#define SKIP_EFI_TEST
-
 #define CONFIG_MACH_TYPE		MACH_TYPE_VENTANA
-
-/* Environment in eMMC, at the end of 2nd "boot sector" */
 
 #include "tegra-common-post.h"
 #include "tegra-common-usb-gadget.h"
